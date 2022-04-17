573deea7dae659d2716757135ad5e7e8a049e62c
idov31 <62358580+Idov31@users.noreply.github.com>
2022-04-17 12:36:16 +0300

    Added UserAssist artifact

--- Modules/Registry.psm1
+++ Modules/Registry.psm1
@@ -11,6 +11,7 @@ function Clear-Registry {
         $runAsUser
     )
     $result = $true
+    Clear-UserAssist $time $users
 
     if (!$runAsUser) {
         if (!$(Clear-BamKey $time $users)) {
@@ -63,6 +64,58 @@ function Clear-BamKey {
     Invoke-RevertToSelf
 
     return $true
+}
+
+function Clear-UserAssist {
+    param (
+        [DateTime]
+        $time,
+
+        [String[]]
+        $users
+    )
+
+    # Registring the HKEY_USERS hive.
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+    $userAssistKeyPath = "Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist"
+
+    foreach ($user in $users) {
+        $sid = $(New-Object System.Security.Principal.NTAccount($user)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+
+        # Checking if the user has user assist key.
+        if (!(Test-Path "HKU:\$($sid)\$($userAssistKeyPath)")) {
+            continue
+        }
+        $userAssistKey = Get-Item "HKU:\$($sid)\$($userAssistKeyPath)"
+
+        # Searching for values created within the range of the timespan.
+        foreach ($subKeyName in $userAssistKey.GetSubKeyNames()) {
+            $currentUserAssistKey = Get-Item "HKU:\$($sid)\$($userAssistKeyPath)\$($subKeyName)\Count"
+
+            foreach ($valueName in $currentUserAssistKey.GetValueNames()) {
+                if ($valueName -eq "HRZR_PGYFRFFVBA") {
+                    continue
+                }
+
+                $rawTimestamp = $userAssistKey.GetValue($valueName)
+
+                # To cover the Windows 7 and Windows 7 and onwards versions.
+                if ($rawTimestamp.Length -gt 68) {
+                    $timestamp = Get-Date ([DateTime]::FromFileTime([bitconverter]::ToInt64($rawTimestamp,60)))
+                }
+                else {
+                    $timestamp = Get-Date ([DateTime]::FromFileTime([bitconverter]::ToInt64($rawTimestamp,8)))
+                }
+
+                $delta = $timestamp - $time
+                
+                if ($delta -gt 0) {
+                    Remove-ItemProperty -Path "HKU:\$($sid)\$($userAssistKeyPath)\$($subKeyName)\Count" -Name $valueName
+                }
+            }
+        }
+    }
+    Write-Host "[+] Removed user assist artifacts!" -ForegroundColor Green
 }
 
 Export-ModuleMember -Function Clear-Registry
